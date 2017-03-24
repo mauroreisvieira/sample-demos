972d07d2c09f6abdbf986bf9da213da4fba9a7ca
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2017-03-24 00:24:14 +0000

    - Removed console log :)

--- cookies/cookie.js
+++ cookies/cookie.js
@@ -20,26 +20,28 @@ class Cookie {
 
 
     setCookie() {
-        if (this._isCookie) {
+
+        if (this._isCookie)
             return;
-        }
+
         this._isCookie = true;
         this.createCookie('cookie', true);
         this._content.classList.add('hidden');
     }
 
     cleanCookie() {
-        if (!this._isCookie) {
+
+        if (!this._isCookie)
             return;
-        }
+
         this._isCookie = false;
         this.createCookie('cookie', false);
         this._content.classList.remove('hidden');
     }
 
     check(cookie) {
+
         let _cookie = this.readCookie(cookie);
-        console.log(_cookie);
         if (_cookie == 'true') {
             this._isCookie = true;
             this._content.classList.add('hidden');
@@ -50,23 +52,30 @@ class Cookie {
     }
 
     createCookie(name,value,days) {
+
         if (days) {
             const cookieLive = 24 * 60 * 60 * 1000;
             var date = new Date();
             date.setTime(date.getTime() + ( days * cookieLive));
-            var expires = "; expires="+date.toGMTString();
+            var expires = "; expires=" + date.toGMTString();
         }
-        else var expires = "";
-        document.cookie = name+"="+value+expires+"; path=/";
+        else {
+            var expires = "";  
+        } 
+        document.cookie = name + "=" + value + expires + "; path=/";
     };
 
     readCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        let nameEQ = name + "=";
+        let cookie = document.cookie.split(';');
+        for (let i = 0; i < cookie.length; i++) {
+            let c = cookie[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1, c.length);
+            }
+            if (c.indexOf(nameEQ) == 0) {
+                return c.substring(nameEQ.length, c.length);  
+            } 
         }
         return null;
     }
