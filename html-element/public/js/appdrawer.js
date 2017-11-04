28c11046861c8ea18802903a5387f57b0d05a85d
Mauro Vieira <mv010690@gmail.com>
2017-11-04 23:51:19 +0000

    - Added HTML Element.

--- html-element/public/js/appdrawer.js
+++ html-element/public/js/appdrawer.js
@@ -1,27 +1,35 @@
-class AppDrawer extends HTMLElement {
+'use strict';
 
-    // Can define constructor arguments if you wish.
-    constructor() {
+var AppDrawer = (function (HTMLElement) {
+    function AppDrawer() {
+        var this$1 = this;
+
         // If you define a ctor, always call super() first!
         // This is specific to CE and required by the spec.
-        super();
+        HTMLElement.call(this);
 
         // Setup a click listener on <app-drawer> itself.
-        this.addEventListener('click', e => {
+        this.addEventListener('click', function (e) {
             // Don't toggle the drawer if it's disabled.
-            if (this.disabled) {
+            if (this$1.disabled) {
                 return;
             }
-            this.toggleDrawer();
+            this$1.toggleDrawer();
         });
     }
 
-    // A getter/setter for an open property.
-    get open() {
-        return this.hasAttribute('open');
-    }
+    if ( HTMLElement ) AppDrawer.__proto__ = HTMLElement;
+    AppDrawer.prototype = Object.create( HTMLElement && HTMLElement.prototype );
+    AppDrawer.prototype.constructor = AppDrawer;
 
-    set open(val) {
+    var prototypeAccessors = { open: {},disabled: {} };
+
+    // A getter/setter for an open property.
+    prototypeAccessors.open.get = function () {
+        return this.hasAttribute('open');
+    };
+
+    prototypeAccessors.open.set = function (val) {
         // Reflect the value of the open property as an HTML attribute.
         if (val) {
             this.setAttribute('open', '');
@@ -29,25 +37,30 @@ class AppDrawer extends HTMLElement {
             this.removeAttribute('open');
         }
         this.toggleDrawer();
-    }
+    };
 
     // A getter/setter for a disabled property.
-    get disabled() {
+    prototypeAccessors.disabled.get = function () {
         return this.hasAttribute('disabled');
-    }
+    };
 
-    set disabled(val) {
+    prototypeAccessors.disabled.set = function (val) {
         // Reflect the value of the disabled property as an HTML attribute.
         if (val) {
             this.setAttribute('disabled', '');
         } else {
             this.removeAttribute('disabled');
         }
-    }
+    };
 
-    toggleDrawer() {
+    AppDrawer.prototype.toggleDrawer = function toggleDrawer () {
         console.log("toggleDrawer");
-    }
-}
+    };
+
+    Object.defineProperties( AppDrawer.prototype, prototypeAccessors );
+
+    return AppDrawer;
+}(HTMLElement));
 
 customElements.define('app-drawer', AppDrawer);
+//# sourceMappingURL=appdrawer.js.map
