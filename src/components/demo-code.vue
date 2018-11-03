18e9d7c10d433238ef3ce32e09ef9b90113913f9
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2018-11-03 00:57:28 +0000

    - added vue file.

---
+++ src/components/demo-code.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="demo-code">
+<pre class="hljs">
+    <code class="language-js">
+        <slot></slot>
+    </code>
+</pre>
+    </div>
+</template>
+
+<script>
+    import  Prism from 'prismjs'
+    export default {
+        name: 'demo-code',
+        mounted () {
+            Prism.highlightAll();
+        }
+    }
+</script>
