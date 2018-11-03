a06f4b9abc6a9bbf97c035295b850e6dcfe82e00
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2018-11-03 00:57:20 +0000

    - added demo code component.

--- src/pages/javascript/break-out-of-nested-loops.vue
+++ src/pages/javascript/break-out-of-nested-loops.vue
@@ -5,47 +5,40 @@
             <div class="card">
                 <h2>Breaking nested loops in JavaScript</h2>
                 <p>Will break from all the nested loops and the parent and stop searching through the array unnecessarily.</p>
-                <div class="demo-code">
-                    <pre class="hljs">
-                        <code class="language-js">
-    const width = 3;
-    const height = 4;
+                <demo-code>
+                    const width = 3;
+                    const height = 4;
 
-    for (let x = 1; x <= width; x++) {
-        for (let y = 1; y <= height; y++) {
-            if (x === 2 && y === 3) {
-                break;
-            }
-            console.log(x + ', ' + y);
-        }
-    }
+                    for (let x = 1; x <= width; x++) {
+                        for (let y = 1; y <= height; y++) {
+                            if (x === 2 && y === 3) {
+                                break;
+                            }
+                            console.log(x + ', ' + y);
+                        }
+                    }
 
-    outerLoop:
-    for (let x = 1; x <= width; x++) {
-        for (let y = 1; y <= height; y++) {
-            if (x === 2 && y === 3) {
-                break outerLoop;
-            }
-            console.warn(x + ', ' + y);
-        }
-    }
-                        </code>
-                    </pre>
-                </div>
+                    outerLoop:
+                    for (let x = 1; x <= width; x++) {
+                        for (let y = 1; y <= height; y++) {
+                            if (x === 2 && y === 3) {
+                                break outerLoop;
+                            }
+                            console.warn(x + ', ' + y);
+                        }
+                    }
+                </demo-code>
             </div>
         </main>
     </div>
 </template>
 
 <script>
-    import  Prism from 'prismjs'
     import hero from './../../layout/hero.vue'
+    import demoCode from './../../components/demo-code.vue'
     export default {
         name: 'relative-time-format',
-        components: { hero },
-            mounted () {
-                Prism.highlightAll();
-            }
+        components: { hero, demoCode }
     }
 </script>
 
--- src/pages/javascript/relative-time-format.vue
+++ src/pages/javascript/relative-time-format.vue
@@ -6,34 +6,27 @@
                 <h2>Relative Time Format API</h2>
                 <p>The brand new <code>Intl.RelativeTimeFormat</code> <strong>API</strong> shifts that burden to the JavaScript engine, which can ship the locale data and make it directly available to JavaScript developers.</p>
                 <p><code>Intl.RelativeTimeFormat</code> enables localized formatting of relative times without sacrificing performance.</p>
-                <div class="demo-code">
-    <pre class="hljs">
-        <code class="language-js">
-    const rtf = new Intl.RelativeTimeFormat('en');
-    rtf.format(3.14, 'second'); // 'in 3.14 seconds'
-    rtf.format(-15, 'minute'); // '15 minutes ago'
-    rtf.format(8, 'hour'); // 'in 8 hours'
-    rtf.format(-2, 'day'); // '2 days ago'
-    rtf.format(3, 'week'); // 'in 3 weeks'
-    rtf.format(-5, 'month'); // '5 months ago'
-    rtf.format(2, 'quarter'); // 'in 2 quarters'
-    rtf.format(-42, 'year'); // '42 years ago'
-        </code>
-    </pre>
-                </div>
+                <demo-code>
+                    const rtf = new Intl.RelativeTimeFormat('en');
+                    rtf.format(3.14, 'second'); // 'in 3.14 seconds'
+                    rtf.format(-15, 'minute'); // '15 minutes ago'
+                    rtf.format(8, 'hour'); // 'in 8 hours'
+                    rtf.format(-2, 'day'); // '2 days ago'
+                    rtf.format(3, 'week'); // 'in 3 weeks'
+                    rtf.format(-5, 'month'); // '5 months ago'
+                    rtf.format(2, 'quarter'); // 'in 2 quarters'
+                    rtf.format(-42, 'year'); // '42 years ago'
+                </demo-code>
             </div>
         </main>
     </div>
 </template>
 
 <script>
-    import  Prism from 'prismjs'
     import hero from './../../layout/hero.vue'
+    import demoCode from './../../components/demo-code.vue'
     export default {
         name: 'relative-time-format',
-        components: { hero },
-            mounted () {
-                Prism.highlightAll();
-            }
+        components: { hero, demoCode }
     }
 </script>
